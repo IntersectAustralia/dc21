c9ed21046d3e834f11fda91ffbf9534d309bae1b
Georgina Edwards <georgina@intersect.org.au>
2011-12-05 12:35:54 +1100

    Fixes to sample data populator

--- lib/tasks/sample_data_populator.rb
+++ lib/tasks/sample_data_populator.rb
@@ -8,6 +8,7 @@ def populate_data
 end
 
 def create_test_files
+  DataFile.delete_all
   DataFile.create!(:format => "file", :path => "/data/dc21-data/sample.txt", :filename => "sample.txt", :created_by => User.first, :start_time => Time.now - 3.days, :end_time => Time.now - 1.day)
   DataFile.create!(:format => "file", :path => "/data/dc21-data/sample2.txt", :filename => "sample2.txt", :created_by => User.first, :start_time => Time.now - 3.years, :end_time => Time.now - 1.year)
   DataFile.create!(:format => "file", :path => "/data/dc21-data/sample3.txt", :filename => "sample3.txt", :created_by => User.last)
@@ -18,18 +19,12 @@ def create_test_users
   create_user(:email => "georgina@intersect.org.au", :first_name => "Georgina", :last_name => "Edwards")
   create_user(:email => "veronica@intersect.org.au", :first_name => "Veronica", :last_name => "Luke")
   create_user(:email => "shuqian@intersect.org.au", :first_name => "Shuqian", :last_name => "Hon")
-  create_user(:email => "alex@intersect.org.au", :first_name => "Alex", :last_name => "Bradner")
-  create_user(:email => "marc@intersect.org.au", :first_name => "Marc", :last_name => "Ziani de Ferranti")
   create_unapproved_user(:email => "unapproved1@intersect.org.au", :first_name => "Unapproved", :last_name => "One")
   create_unapproved_user(:email => "unapproved2@intersect.org.au", :first_name => "Unapproved", :last_name => "Two")
   set_role("sean@intersect.org.au", "Administrator")
   set_role("georgina@intersect.org.au", "Administrator")
   set_role("veronica@intersect.org.au", "Administrator")
-  set_role("raul@intersect.org.au", "Administrator")
-  set_role("diego@intersect.org.au", "Administrator")
   set_role("shuqian@intersect.org.au", "Administrator")
-  set_role("alex@intersect.org.au", "Administrator")
-  set_role("marc@intersect.org.au", "Administrator")
 
 end
 
