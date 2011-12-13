8148d04d52c96807454510bbde4f41f82174cca3
Georgina Edwards <georgina@intersect.org.au>
2011-12-14 09:37:57 +1100

    DC21-83: make parser more robust for cases where file is not valid

--- lib/toa5_parser.rb
+++ lib/toa5_parser.rb
@@ -4,32 +4,10 @@ class Toa5Parser
 
     interesting_lines = extract_interesting_lines(file)
 
-    if interesting_lines[:line_5]
-      data_file.start_time = extract_time_from_data_line(interesting_lines[:line_5])
-    end
+    data_file.start_time = extract_time_from_data_line(interesting_lines[:line_5]) if interesting_lines[:line_5]
+    data_file.end_time = extract_time_from_data_line(interesting_lines[:last_line]) if interesting_lines[:last_line]
 
-    if interesting_lines[:last_line]
-      data_file.end_time = extract_time_from_data_line(interesting_lines[:last_line])
-    end
-
-    if interesting_lines[:line_1]
-      headers = interesting_lines[:line_1].split("\t")
-      station_name = headers[1]
-      model = headers[2]
-      serial_number= headers[3]
-      os_version = headers[4]
-      dld_name = headers[5]
-      dld_signature = headers[6]
-      table_name = headers[7]
-
-      data_file.add_metadata_item(:datalogger_model, model) unless model.blank?
-      data_file.add_metadata_item(:station_name, station_name) unless station_name.blank?
-      data_file.add_metadata_item(:serial_number, serial_number) unless serial_number.blank?
-      data_file.add_metadata_item(:os_version, os_version) unless os_version.blank?
-      data_file.add_metadata_item(:dld_name, dld_name) unless dld_name.blank?
-      data_file.add_metadata_item(:dld_signature, dld_signature) unless dld_signature.blank?
-      data_file.add_metadata_item(:table_name, table_name) unless table_name.blank?
-    end
+    extract_header_line_info(data_file, interesting_lines[:line_1]) if interesting_lines[:line_1]
 
     if interesting_lines[:line_2] && interesting_lines[:line_3] && interesting_lines[:line_4]
       headers = interesting_lines[:line_2].gsub("\n", "").split("\t")
@@ -38,6 +16,25 @@ class Toa5Parser
       data_file.add_metadata_item(:column_headers, extract_column_info(headers, units, col_types))
     end
     data_file.save!
+  end
+
+  def self.extract_header_line_info(data_file, header_line)
+    headers = header_line.split("\t")
+    station_name = headers[1]
+    model = headers[2]
+    serial_number= headers[3]
+    os_version = headers[4]
+    dld_name = headers[5]
+    dld_signature = headers[6]
+    table_name = headers[7]
+
+    data_file.add_metadata_item(:datalogger_model, model) unless model.blank?
+    data_file.add_metadata_item(:station_name, station_name) unless station_name.blank?
+    data_file.add_metadata_item(:serial_number, serial_number) unless serial_number.blank?
+    data_file.add_metadata_item(:os_version, os_version) unless os_version.blank?
+    data_file.add_metadata_item(:dld_name, dld_name) unless dld_name.blank?
+    data_file.add_metadata_item(:dld_signature, dld_signature) unless dld_signature.blank?
+    data_file.add_metadata_item(:table_name, table_name) unless table_name.blank?
   end
 
   def self.extract_column_info(headers, units, col_types)
@@ -49,9 +46,20 @@ class Toa5Parser
   end
 
   def self.extract_time_from_data_line(line)
-    details = line.split("\t")
-    time_string = details.first + " UTC" #force to consider it UTC as we don't know the timezone  
-    Time.parse(time_string)
+    begin
+      details = line.split("\t")
+      time_string = details.first
+      parse_time(time_string)
+    rescue
+      puts "Error parsing date from TOA5 file: #{$!}"
+      ::Rails.logger.info("Error parsing date from TOA5 file: #{$!} (#{time_string}")
+      nil
+    end
+  end
+
+  def self.parse_time(time_string)
+    # Time.parse is too forgiving, so we use DateTime.strptime instead
+    DateTime.strptime(time_string, "%e/%m/%Y %R").to_time
   end
 
   def self.extract_interesting_lines(file)
--- spec/lib/toa5_parser_spec.rb
+++ spec/lib/toa5_parser_spec.rb
@@ -7,6 +7,11 @@ describe Toa5Parser do
     Factory(:data_file, :path => path, :filename => 'toa5.dat')
   end
 
+  let(:not_really_toa5) do
+    path = Rails.root.join('spec/samples', 'not-really-toa5.dat')
+    Factory(:data_file, :path => path, :filename => 'not-really-toa5.dat')
+  end
+
   let(:toa5_dat_with_blanks) do
     path = Rails.root.join('spec/samples', 'toa5_with_blanks.dat')
     Factory(:data_file, :path => path, :filename => 'toa5_with_blanks.dat')
@@ -64,5 +69,32 @@ describe Toa5Parser do
       headers[2].should eq(["BattV_Min", "Volts", "Min"])
       headers[3].should eq(["PTemp_C_Max", "Deg C", "Max"])
     end
+  end
+
+  describe "invalid file" do
+    it "should do nothing if file doesn't conform to expected format" do
+      data_file = not_really_toa5
+      Toa5Parser.extract_metadata(data_file)
+      data_file.start_time.should be_nil
+      data_file.end_time.should be_nil
+      data_file.metadata.should be_empty
+    end
+  end
+
+  describe "Parse time" do
+    it "should parse times that match the format used in TOA5" do
+      Toa5Parser.parse_time("8/10/2011 9:55").to_s.should eq("2011-10-08 09:55:00 UTC")
+      Toa5Parser.parse_time("8/10/2011 10:00").to_s.should eq("2011-10-08 10:00:00 UTC")
+      Toa5Parser.parse_time("12/10/2011 1:50").to_s.should eq("2011-10-12 01:50:00 UTC")
+      Toa5Parser.parse_time("13/10/2011 22:25").to_s.should eq("2011-10-13 22:25:00 UTC")
+    end
+
+    #it "should reject others" do
+    #  Toa5Parser.parse_time("8/10/2011 9:55 AM")
+    #  Toa5Parser.parse_time("8/10/11 9:55")
+    #  Toa5Parser.parse_time("8/13/11 9:55")
+    #  Toa5Parser.parse_time("8/13/11")
+    #  Toa5Parser.parse_time("Junk")
+    #end
   end
 end
