20697972badf395a2e027e9bbd42d57f59cfa2bf
Marc Ziani de Ferranti <marc@intersect.org.au>
2011-12-19 15:12:55 +1100

    sorting icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,8 +44,9 @@ module ApplicationHelper
 
   def sortable(column, title = nil)  
     title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, params.merge(:sort => column, :direction => direction)
+    link_to title, params.merge(:sort => column, :direction => direction), { :class => css_class }
   end
 
 
