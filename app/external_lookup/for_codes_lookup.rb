70a2a85f48256db5cf87b30c792814138b775e97
Georgina Edwards <georgina@intersect.org.au>
2012-02-15 10:29:24 +1100

    DC21-149: code to read second and third level FOR codes from MINT

--- app/external_lookup/for_codes_lookup.rb
+++ app/external_lookup/for_codes_lookup.rb
@@ -1,18 +1,29 @@
 class ForCodesLookup
   include HTTParty
 
-  URL =
-
+  FOR_CODES_PATH = APP_CONFIG['for_codes_path']
   base_uri APP_CONFIG['for_codes_lookup_base_url']
   format :json
 
   def top_level_codes
-    response = self.class.get('/mint/ANZSRC_FOR/opensearch/lookup?level=top&count=9999', :timeout => 300)
+    get_codes("top")
+  end
+
+  def second_level_codes(top_level_code)
+    get_codes(top_level_code)
+  end
+
+  def third_level_codes(second_level_code)
+    get_codes(second_level_code)
+  end
+
+  private
+  def get_codes(level)
+    response = self.class.get(FOR_CODES_PATH, :query => {:level => level, :count => 9999}, :timeout => 1000)
     codes = []
     response["results"].each do |result|
       codes << [result["rdf:about"], result["skos:prefLabel"]]
     end
     codes.sort { |x, y| x[1] <=> y[1] }
   end
-
 end
--- spec/external_lookup/for_codes_lookup_spec.rb
+++ spec/external_lookup/for_codes_lookup_spec.rb
@@ -1,13 +1,29 @@
 require 'spec_helper'
 
+#TODO: currently depends on the real API
 describe ForCodesLookup do
   it "Get top level codes should return an array of top level codes" do
-    #TODO: currently depends on the real API
     lookup = ForCodesLookup.new
     codes = lookup.top_level_codes
     codes.size.should eq(22)
     codes[0].should eq(["http://purl.org/asc/1297.0/2008/for/01", "01 - MATHEMATICAL SCIENCES"])
     codes[6].should eq(["http://purl.org/asc/1297.0/2008/for/07", "07 - AGRICULTURAL AND VETERINARY SCIENCES"])
     codes[21].should eq(["http://purl.org/asc/1297.0/2008/for/22", "22 - PHILOSOPHY AND RELIGIOUS STUDIES"])
+  end
+
+  it "should be able to get second level codes based on a top level code" do
+    lookup = ForCodesLookup.new
+    codes = lookup.second_level_codes("http://purl.org/asc/1297.0/2008/for/07")
+    codes.size.should eq(8)
+    codes[0].should eq(["http://purl.org/asc/1297.0/2008/for/0701", "0701 - Agriculture, Land and Farm Management"])
+    codes[7].should eq(["http://purl.org/asc/1297.0/2008/for/0799", "0799 - Other Agricultural and Veterinary Sciences"])
+  end
+
+  it "should be able to get third level codes based on a second level code" do
+    lookup = ForCodesLookup.new
+    codes = lookup.third_level_codes("http://purl.org/asc/1297.0/2008/for/0701")
+    codes.size.should eq(9)
+    codes[0].should eq(["http://purl.org/asc/1297.0/2008/for/070101", "070101 - Agricultural Land Management"])
+    codes[8].should eq(["http://purl.org/asc/1297.0/2008/for/070199", "070199 - Agriculture, Land and Farm Management not elsewhere classified"])
   end
 end
