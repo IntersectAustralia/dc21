efc56416d2fab9d18fa64ad75fe9c33b4fdc1cdb
Georgina Edwards <gc_edwards@yahoo.com.au>
2012-05-30 11:42:36 +1000

    DC21-215: move values for seeding into a YAML file

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -4,15 +4,18 @@ def create_roles
   Role.create!(:name => "Administrator")
   Role.create!(:name => "Researcher")
   Role.create!(:name => "API Uploader")
-
 end
 
 def create_tags
   Tag.delete_all
-  Tag.create!(:name => "Photo")
-  Tag.create!(:name => "Video")
-  Tag.create!(:name => "Gap Filled")
+
+  config_file = File.expand_path('../../config/tags.yml', __FILE__)
+  config = YAML::load_file(config_file)
+  config['tags'].each do |hash|
+    Tag.create!(hash)
+  end
 end
+
 
 def create_parameter_categories
   ParameterCategory.delete_all
@@ -20,60 +23,22 @@ def create_parameter_categories
   ParameterModification.delete_all
   ParameterUnit.delete_all
 
-  light = ParameterCategory.create!(name: "Light")
-  atmosphere = ParameterCategory.create!(name: "Atmosphere")
-  temperature = ParameterCategory.create!(name: "Temperature")
-  humidity = ParameterCategory.create!(name: "Humidity")
-  soil = ParameterCategory.create!(name: "Soil")
-  nutrients = ParameterCategory.create!(name: "Nutrients")
-  water = ParameterCategory.create!(name: "Water")
+  config_file = File.expand_path('../../config/experiment_parameters.yml', __FILE__)
+  config = YAML::load_file(config_file)
 
-  atmosphere.parameter_sub_categories.create!(name: "Carbon Dioxide")
-  atmosphere.parameter_sub_categories.create!(name: "Nitrogen")
-  atmosphere.parameter_sub_categories.create!(name: "Oxygen")
+  config['parameter_categories'].each do |hash|
+    sub_categories = hash.delete('sub_categories')
+    pc = ParameterCategory.create!(hash)
+    sub_categories.each do |psc|
+      pc.parameter_sub_categories.create!(psc)
+    end
+  end
 
-  light.parameter_sub_categories.create!(name: "Natural")
-  light.parameter_sub_categories.create!(name: "Infrared")
-  light.parameter_sub_categories.create!(name: "Ultraviolet")
+  config['parameter_units'].each do |hash|
+    ParameterUnit.create!(hash)
+  end
 
-  temperature.parameter_sub_categories.create!(name: "Air temperature")
-  temperature.parameter_sub_categories.create!(name: "Soil temperature")
-
-  humidity.parameter_sub_categories.create!(name: "Normal")
-
-  soil.parameter_sub_categories.create!(name: "Entisol")
-  soil.parameter_sub_categories.create!(name: "Vertisol")
-  soil.parameter_sub_categories.create!(name: "Inceptisol")
-  soil.parameter_sub_categories.create!(name: "Aridisol")
-  soil.parameter_sub_categories.create!(name: "Mollisol")
-  soil.parameter_sub_categories.create!(name: "Spodosol")
-  soil.parameter_sub_categories.create!(name: "Alfisol")
-  soil.parameter_sub_categories.create!(name: "Ultisol")
-  soil.parameter_sub_categories.create!(name: "Oxisol")
-  soil.parameter_sub_categories.create!(name: "Histosol")
-  soil.parameter_sub_categories.create!(name: "Andisols")
-  soil.parameter_sub_categories.create!(name: "Gelisols")
-
-  nutrients.parameter_sub_categories.create!(name: "Calcium")
-  nutrients.parameter_sub_categories.create!(name: "Phosphorus")
-
-  water.parameter_sub_categories.create!(name: "Rain water")
-  water.parameter_sub_categories.create!(name: "Tap water")
-  water.parameter_sub_categories.create!(name: "Distilled water")
-
-  ParameterModification.create!(name: "Above ambient")
-  ParameterModification.create!(name: "Below ambient")
-  ParameterModification.create!(name: "Absolute target")
-  ParameterModification.create!(name: "Excluded")
-
-  ParameterUnit.create!(:name => "L/m2")
-  ParameterUnit.create!(:name => "Percentage")
-  ParameterUnit.create!(:name => "PPM")
-  ParameterUnit.create!(:name => "Degrees C")
-  ParameterUnit.create!(:name => "Lumens")
-  ParameterUnit.create!(:name => "Litres")
-  ParameterUnit.create!(:name => "Millilitres")
-  ParameterUnit.create!(:name => "Grams/Cubic Metre")
-  ParameterUnit.create!(:name => "Kilograms/Cubic Metre")
-
+  config['parameter_modifications'].each do |hash|
+    ParameterModification.create!(hash)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+unless %w(qa development test staging).include?(Rails.env)
+  if Tag.first.present? || ParameterCategory.first.present? || Role.first.present?
+    puts "----------\nYou cannot reseed an existing live deployment!\nIf something has genuinely gone wrong then this is not the way to fix it\nConsider lodging a support ticket.\n----------"
+    raise StandardError, "FATAL: running db:seed more than once on a live deployment"
+  end
+end
+
 require File.dirname(__FILE__) + '/seed_helper.rb'
 
 create_roles
-
 create_parameter_categories
-
 create_tags
