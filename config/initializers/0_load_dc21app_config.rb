90de5833903749166228830b9d7dc0dd2648f519
Shuqian Hon <sq@intersect.org.au>
2012-07-27 16:32:08 +1000

    DC21-262
    Trying out new config deploy process.

--- config/initializers/0_load_dc21app_config.rb
+++ config/initializers/0_load_dc21app_config.rb
@@ -1,3 +1,8 @@
 APP_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/dc21app_config.yml")[Rails.env]
-extra_config = YAML.load_file("#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.yml")[Rails.env]
-APP_CONFIG.merge!(extra_config)
+
+if File.exists?("#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.yml")
+  extra_config = YAML.load_file("#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.yml")[Rails.env]
+  APP_CONFIG.merge!(extra_config)
+else
+  puts "#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.yml doesn't exist yet. Some thing will NOT work".red
+end
