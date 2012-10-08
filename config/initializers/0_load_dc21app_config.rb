94f9bca8464840154dd6d75921b855233c158edd
Georgina Edwards <gc_edwards@yahoo.com.au>
2012-10-08 16:43:51 +1100

    Change confusing message coming from config loading

--- config/initializers/0_load_dc21app_config.rb
+++ config/initializers/0_load_dc21app_config.rb
@@ -4,5 +4,5 @@ if File.exists?("#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.ym
   extra_config = YAML.load_file("#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.yml")[Rails.env]
   APP_CONFIG.merge!(extra_config)
 else
-  puts "#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.yml doesn't exist yet. Some things will NOT work".red
+  puts "#{APP_CONFIG['extra_config_file_root']}/dc21app_extra_config.yml doesn't exist yet. This is ok in a local dev environment but should not be the case in production".yellow
 end
