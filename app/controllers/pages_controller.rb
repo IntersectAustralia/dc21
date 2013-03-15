483dcaf34cf8dddb26af61ad270e911a0778f4a8
charles <charles@intersect.org.au>
2013-03-15 18:06:38 +1100

    Remove debug code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,20 +22,4 @@ class PagesController < ApplicationController
       render :layout => 'data_files'
     end
   end
-
-  def route_404
-    raise ActionController::RoutingError.new('Not Found')
-    #render :file => "#{Rails.root}/public/404.html", :status => 404
-  end
-
-  def route_422
-    render :file => "#{Rails.root}/public/422.html", :status => 422
-  end
-
-  def route_500
-    render :file => "#{Rails.root}/public/500.html", :status => 500
-  end
-
-  private
-
 end
