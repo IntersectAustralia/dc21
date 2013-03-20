a607e81d6599d49fd9b659a6f1735929ea8417fd
Shuqian Hon <sq@intersect.org.au>
2013-03-20 13:18:30 +1100

    DC21-457: Fix facility ordering in dropdowns

--- app/helpers/data_files_helper.rb
+++ app/helpers/data_files_helper.rb
@@ -1,13 +1,6 @@
 module DataFilesHelper
 
   def grouped_experiments_for_select
-    facilities = Facility.order(:name).all
-    other = Facility.find_by_name('Other')
-    if other
-      facilities.delete(other)
-      facilities << other
-    end
-
-    facilities
+    Facility.order(:name).all
   end
 end
