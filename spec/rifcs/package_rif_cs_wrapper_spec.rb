1cbb40d266a1e10f8b72c0777c1ea4182c46e27c
charles <charles@intersect.org.au>
2013-03-28 17:19:57 +1100

    Fix rspec

--- spec/rifcs/package_rif_cs_wrapper_spec.rb
+++ spec/rifcs/package_rif_cs_wrapper_spec.rb
@@ -31,17 +31,13 @@ describe PackageRifCsWrapper do
   end
 
   describe "Notes" do
-      package = Factory(:package,
-                          filename: 'mypackage.zip',
-                          id: 3,
-                          experiment_id: 4,
-                          file_processing_status: 'PACKAGE',
-                          format: "BAGIT",
-                          created_at: "2012-12-27 14:09:24",
-                          file_processing_description: "This package contains a lot of cats. Be warned.",
-                          created_by: Factory(:user))
 
     it "should include submitter in notes" do
+      user = Factory(:user, :first_name => "postman", :last_name => "pac", :email => "postmanpac@intersect.org.au")
+      post_facility = Factory(:facility, :name =>'PFac', :primary_contact => user)
+      experiment = Factory(:experiment, :facility => post_facility)
+      package = Factory(:package, filename: 'notepackage.zip', experiment_id: experiment.id, file_processing_status: 'PACKAGE', format: "BAGIT", created_at: "2012-12-27 14:09:24",
+                          file_processing_description: "This package contains a lot of cats. Be warned.", created_by: user)
       wrapper = PackageRifCsWrapper.new(package, [], {:submitter => Factory(:user, :email => "georgina@intersect.org.au", :first_name => "Georgina", :last_name => "Edwards")})
       wrapper.notes[0].should eq('Published by Georgina Edwards (georgina@intersect.org.au)')
     end
@@ -59,6 +55,9 @@ describe PackageRifCsWrapper do
       df2 = Factory(:data_file, :experiment_id => experiment2.id)
       df3 = Factory(:data_file, :experiment_id => experiment3.id)
       df4 = Factory(:data_file, :experiment_id => experiment3.id)
+
+      package = Factory(:package, filename: 'notepackage.zip', experiment_id: experiment1.id, file_processing_status: 'PACKAGE', format: "BAGIT", created_at: "2012-12-27 14:09:24",
+                    file_processing_description: "This package contains a lot of cats. Be warned.", created_by: user1)
       wrapper = PackageRifCsWrapper.new(package, [df1, df2, df3, df4], {:submitter => Factory(:user, :email => "georgina@intersect.org.au", :first_name => "Georgina", :last_name => "Edwards")})
       wrapper.notes.size.should eq(3)
       wrapper.notes.include?('Primary contact for Fac1 is Fred Smith (fred@intersect.org.au')
@@ -73,6 +72,9 @@ describe PackageRifCsWrapper do
       experiment = Factory(:experiment, :facility => facility)
 
       df1 = Factory(:data_file, :experiment_id => experiment.id)
+      package = Factory(:package, filename: 'notepackage.zip', experiment_id: experiment.id, file_processing_status: 'PACKAGE', format: "BAGIT", created_at: "2012-12-27 14:09:24",
+                    file_processing_description: "This package contains a lot of cats. Be warned.", created_by: user)
+
       wrapper = PackageRifCsWrapper.new(package, [df1], {:submitter => Factory(:user, :email => "georgina@intersect.org.au", :first_name => "Georgina", :last_name => "Edwards")})
       # this should never happen, its ok that nothing shows
       wrapper.notes.size.should eq(1)
