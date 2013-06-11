bd2809d48d477d017a5d458bcc457f163fd2e30e
Georgina Edwards <gc_edwards@yahoo.com.au>
2013-06-12 09:12:39 +1000

    Lock passenger version so we don't have unpredictable results from installing

--- config/deploy.rb
+++ config/deploy.rb
@@ -93,7 +93,7 @@ namespace :server_setup do
   end
   task :gem_install, :roles => :app do
     run "gem install bundler -v 1.0.20"
-    run "gem install passenger"
+    run "gem install passenger -v 3.0.21"
   end
   task :passenger, :roles => :app do
     run "passenger-install-apache2-module -a"
