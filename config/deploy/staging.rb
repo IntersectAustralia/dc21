dbd0c09d8984c63eb93134a7efe98a6e0aed9b64
danieltheodosius <danielt@intersect.org.au>
2013-08-28 10:17:14 +1000

    DEVSUPPORT-1078 - change the staging server url

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
 # Your HTTP server, Apache/etc
-set :web_server, 'jp-dc21-staging.intersect.org.au'
+set :web_server, 'ic2-dc21-staging-vm.intersect.org.au'
 # This may be the same as your Web server
-set :app_server, 'jp-dc21-staging.intersect.org.au'
+set :app_server, 'ic2-dc21-staging-vm.intersect.org.au'
 # This is where Rails migrations will run
-set :db_server, 'jp-dc21-staging.intersect.org.au'
+set :db_server, 'ic2-dc21-staging-vm.intersect.org.au'
 # The user configured to run the rails app
 set :user, 'devel'
 
