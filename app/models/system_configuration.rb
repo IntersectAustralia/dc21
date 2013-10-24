74b10edbd526c3d5bd35d64a81122578b0cc3162
Cindy <cindy@intersect.org.au>
2013-10-24 17:58:36 +1100

    EYETRACKER-1: Fix bugs to allow blank email, check levels 1 & 2 fields not same, set length to optional fields.

--- app/models/system_configuration.rb
+++ app/models/system_configuration.rb
@@ -1,20 +1,34 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
-
 class SystemConfiguration < ActiveRecord::Base
 
   acts_as_singleton
   validates_presence_of :level1, :level1_plural, :level2, :level2_plural
+  validates_length_of :level1, :level1_plural, :level2, :level2_plural, :maximum=>20
+  validates_length_of :address1, :address2, :address3, :telephone_number, :urls, :maximum=>80
+  validates_length_of :description, :maximum=>10000
 
   validates :name, presence:true, length: {maximum: 20}
   validates :research_centre_name, presence:true, length: {maximum: 80}
   validates :entity, presence: true, length: {maximum: 80}
-
-  validates :email, email:true
+  validates :email, length: {maximum: 80}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, allow_blank: true
   #validates :telephone_number, format: { with: /\A[+]?[\s\d]+\Z/, message: "is not a valid phone number"}
+
+  validate :level2_cannot_equal_level1_fields
+
+  def level2_cannot_equal_level1_fields
+    if self.level1 != '' && self.level1 == self.level1_plural
+      errors.add(:level1_plural, "cannot be the same as Level 1 singular")
+    end
+
+    if self.level2 != '' && self.level2 == self.level2_plural
+      errors.add(:level2_plural, "cannot be the same as Level 2 singular")
+    end
+
+    if self.level1 == self.level2 || self.level1 == self.level2_plural
+      errors.add(:level1, "singular cannot be the same as Level 2 singular or plural")
+    elsif self.level1_plural == self.level2 || self.level1_plural == self.level2_plural
+      errors.add(:level1_plural, "cannot be the same as Level 2 singular or plural")
+    end
+  end
+
 end
--- spec/models/system_config_spec.rb
+++ spec/models/system_config_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe SystemConfiguration do
 #EYETRACKER-1
+  long_name = 'a' * 81
+
   describe "Update system config fields" do
     it "should fail if no local system name is given" do
       config = SystemConfiguration.instance
@@ -24,6 +26,28 @@ describe SystemConfiguration do
       config.errors[:entity].should eq ["can't be blank"]
     end
 
+    it "should fail if no organisational level 1 is given" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:level1 => ""})
+      result.should be_false
+      config.errors[:level1].should eq ["can't be blank"]
+
+      result = config.update_attributes({:level1_plural => ""})
+      result.should be_false
+      config.errors[:level1_plural].should eq ["can't be blank"]
+    end
+
+    it "should fail if no organisational level 2 is given" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:level2 => ""})
+      result.should be_false
+      config.errors[:level2].should eq ["can't be blank"]
+
+      result = config.update_attributes({:level2_plural => ""})
+      result.should be_false
+      config.errors[:level2_plural].should eq ["can't be blank"]
+    end
+
     it "should fail if local system name is longer than 20 characters" do
       config = SystemConfiguration.instance
       result = config.update_attributes({:name => "qwertyuioplkjhgfdsazx"})
@@ -33,7 +57,6 @@ describe SystemConfiguration do
 
     it "should fail if research centre name is longer than 80 characters" do
       config = SystemConfiguration.instance
-      long_name = 'a' * 81
       result = config.update_attributes({:research_centre_name => long_name})
       result.should be_false
       config.errors[:research_centre_name].should eq ["is too long (maximum is 80 characters)"]
@@ -43,15 +66,96 @@ describe SystemConfiguration do
       config = SystemConfiguration.instance
       result = config.update_attributes({:name => '<>?/.,:', :description => '+_)(*&^%$#@!`~17-=][{}|:;?>\\<,./`'})
       result.should be_true
+      config.errors[:email].should eq []
     end
 
     it "should fail if email is not valid" do
       config = SystemConfiguration.instance
       result = config.update_attributes({:email => 'invalid_email'})
       result.should be_false
-      config.errors[:email].should eq ["is not an email"]
+      config.errors[:email].should eq ["is invalid"]
     end
 
+    it "should pass if email is blank" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:email => ''})
+      result.should be_true
+      config.errors[:email].should eq []
+    end
+
+    it "should fail if org level2 value equals level1" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:level1 => 'same name', :level2 => 'same name'})
+      result.should be_false
+      config.errors[:level1].should eq ["singular cannot be the same as Level 2 singular or plural"]
+
+      result = config.update_attributes({:level1_plural => 'same name'})
+      result.should be_false
+      config.errors[:level1_plural].should eq ["cannot be the same as Level 1 singular"]
+
+      result = config.update_attributes({:level2_plural => 'same name'})
+      result.should be_false
+      config.errors[:level2_plural].should eq ["cannot be the same as Level 2 singular"]
+    end
+
+    it "should fail if a line in the address field is longer than 80 characters" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:address1 => long_name})
+      result.should be_false
+      config.errors[:address1].should eq ["is too long (maximum is 80 characters)"]
+
+      result = config.update_attributes({:address2 => long_name})
+      result.should be_false
+      config.errors[:address2].should eq ["is too long (maximum is 80 characters)"]
+
+      result = config.update_attributes({:address3 => long_name})
+      result.should be_false
+      config.errors[:address3].should eq ["is too long (maximum is 80 characters)"]
+    end
+
+    it "should pass if address is blank" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:address1 => ''})
+      result.should be_true
+      config.errors[:address1].should eq []
+
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:address2 => ''})
+      result.should be_true
+      config.errors[:address2].should eq []
+
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:address3 => ''})
+      result.should be_true
+      config.errors[:address3].should eq []
+    end
+    it "should fail if URL is longer than 80 characters" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:urls => long_name})
+      result.should be_false
+      config.errors[:urls].should eq ["is too long (maximum is 80 characters)"]
+    end
+
+    it "should pass if URLs is blank" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:urls => ''})
+      result.should be_true
+      config.errors[:urls].should eq []
+    end
+
+    it "should pass if telephone number is blank" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:telephone_number => ''})
+      result.should be_true
+      config.errors[:telephone_number].should eq []
+    end
+
+    it "should pass if description is blank" do
+      config = SystemConfiguration.instance
+      result = config.update_attributes({:description => ''})
+      result.should be_true
+      config.errors[:description].should eq []
+    end
     #it "should fail if telephone is not a number" do
     #  config = SystemConfiguration.instance
     #  result = config.update_attributes({:telephone_number => 'not a number'})
