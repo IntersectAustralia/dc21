3986d98b271b18f536759d1f3b275534b39eabd3
Shuqian Hon <sq@intersect.org.au>
2013-11-12 19:01:29 +1100

    EYETRACKER-93: Add Tesseract library install

--- vm_setup.sh
+++ vm_setup.sh
@@ -22,6 +22,7 @@ if [ "$status" -ne 0 ]; then
   exit $status
 fi
 
+#Installs standard RPMs
 sudo rpm -Uvh http://mirrors.kernel.org/fedora-epel/6/i386/epel-release-6-8.noarch.rpm http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
 sudo yum install -y gcc gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel unzip libffi-devel openssl openssl-devel make bzip2 autoconf automake libtool bison httpd httpd-devel apr-devel apr-util-devel mod_ssl mod_xsendfile curl curl-devel openssl openssl-devel tzdata libxml2 libxml2-devel libxslt libxslt-devel sqlite-devel git postgresql-server postgresql postgresql-devel
@@ -30,6 +31,22 @@ sudo setenforce 0
 rm -rf $HOME/code_base
 mkdir $HOME/code_base
 cd $HOME/code_base
+
+# Installs Tesseract OCR
+sudo yum -y install libjpeg-devel libpng-devel libtiff-devel zlib-devel
+sudo yum -y install gcc gcc-c++ make
+wget http://www.leptonica.com/source/leptonica-1.69.tar.gz
+wget http://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.02.tar.gz
+wget http://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
+tar zxvf leptonica-1.69.tar.gz
+tar zxvf tesseract-ocr-3.02.02.tar.gz
+tar zxvf tesseract-ocr-3.02.eng.tar.gz
+
+cd $HOME/code_base/leptonica-1.69 && ./configure && make && sudo make install
+
+cd $HOME/code_base/tesseract-ocr/ && ./autogen.sh && ./configure && make && sudo make install && sudo mv $HOME/code_base/tesseract-ocr/tessdata/eng.* /usr/local/share/tessdata/
+sudo ldconfig
+
 wget https://github.com/IntersectAustralia/dc21/archive/$DC21_TAG.zip -O $HOME/code_base/dc21.zip
 unzip $HOME/code_base/dc21.zip
 rm $HOME/code_base/dc21.zip
