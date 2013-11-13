2f994abec45a1afaef5c5c0107a49d5be8f15590
Shuqian Hon <sq@intersect.org.au>
2013-11-13 15:29:12 +1100

    EYETRACKER-93: Add more error checking in vm_setup.sh

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -1,27 +1,30 @@
 namespace :postgresql do
 
   task :setup do
-    run %Q{#{try_sudo} service postgresql initdb}
+    begin
+      run %Q{#{try_sudo} service postgresql initdb}
 
-    #refresh pg_hba.conf
+      #refresh pg_hba.conf
 
-    run %Q{#{try_sudo} truncate -s0 /var/lib/pgsql/data/pg_hba.conf}
-    run %Q{echo "local all all  trust" | #{try_sudo} tee -a /var/lib/pgsql/data/pg_hba.conf}
-    run %Q{echo "host all all 127.0.0.1/32 password" | #{try_sudo} tee -a /var/lib/pgsql/data/pg_hba.conf}
-    run %Q{echo "host all all ::1/128 password" | #{try_sudo} tee -a /var/lib/pgsql/data/pg_hba.conf}
+      run %Q{#{try_sudo} truncate -s0 /var/lib/pgsql/data/pg_hba.conf}
+      run %Q{echo "local all all  trust" | #{try_sudo} tee -a /var/lib/pgsql/data/pg_hba.conf}
+      run %Q{echo "host all all 127.0.0.1/32 password" | #{try_sudo} tee -a /var/lib/pgsql/data/pg_hba.conf}
+      run %Q{echo "host all all ::1/128 password" | #{try_sudo} tee -a /var/lib/pgsql/data/pg_hba.conf}
 
-    sudo "service postgresql restart"
-    sudo "chkconfig postgresql on"
+      sudo "service postgresql restart"
+      sudo "chkconfig postgresql on"
 
-    buffer = YAML::load_file('config/database.yml')
+      buffer = YAML::load_file('config/database.yml')
 
-    postgresql_user = buffer[rails_env]['username']
-    postgresql_password = buffer[rails_env]['password']
-    postgresql_database = buffer[rails_env]['database']
+      postgresql_user = buffer[rails_env]['username']
+      postgresql_password = buffer[rails_env]['password']
+      postgresql_database = buffer[rails_env]['database']
 
-    sudo %Q{psql -c "create user #{postgresql_user} with SUPERUSER password '#{postgresql_password}';"}, as: "postgres"
-    sudo %Q{psql -c "create database #{postgresql_database};"}, as: "postgres"
-
+      sudo %Q{psql -c "create user #{postgresql_user} with SUPERUSER password '#{postgresql_password}';"}, as: "postgres"
+      sudo %Q{psql -c "create database #{postgresql_database};"}, as: "postgres"
+    rescue Capistrano::Error => error
+      puts "Error occured while setting up Postgres database: #{error}"
+    end
   end
 
 end
--- vm_setup.sh
+++ vm_setup.sh
@@ -34,27 +34,35 @@ mkdir $HOME/code_base
 cd $HOME/code_base
 
 # Installs Tesseract OCR
-sudo yum -y install libjpeg-devel libpng-devel libtiff-devel zlib-devel
-sudo yum -y install gcc gcc-c++ make
-wget http://www.leptonica.com/source/leptonica-1.69.tar.gz
-wget http://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.02.tar.gz
-wget http://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
-tar zxvf leptonica-1.69.tar.gz
-tar zxvf tesseract-ocr-3.02.02.tar.gz
-tar zxvf tesseract-ocr-3.02.eng.tar.gz
+type -P tesseract > /dev/null
+if [ $? -ne 0 ]; then
+  sudo yum -y install libjpeg-devel libpng-devel libtiff-devel zlib-devel
+  sudo yum -y install gcc gcc-c++ make
+  wget http://www.leptonica.com/source/leptonica-1.69.tar.gz
+  wget http://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.02.tar.gz
+  wget http://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
+  tar zxvf leptonica-1.69.tar.gz
+  tar zxvf tesseract-ocr-3.02.02.tar.gz
+  tar zxvf tesseract-ocr-3.02.eng.tar.gz
 
-cd $HOME/code_base/leptonica-1.69 && ./configure && make && sudo make install
+  cd $HOME/code_base/leptonica-1.69 && ./configure && make && sudo make install
 
-cd $HOME/code_base/tesseract-ocr/ && ./autogen.sh && ./configure && make && sudo make install && sudo mv $HOME/code_base/tesseract-ocr/tessdata/eng.* /usr/local/share/tessdata/
-sudo ldconfig
+  cd $HOME/code_base/tesseract-ocr/ && ./autogen.sh && ./configure && make && sudo make install && sudo mv $HOME/code_base/tesseract-ocr/tessdata/eng.* /usr/local/share/tessdata/
+  sudo ldconfig
+fi
 
 cd $HOME/code_base
 git clone git://github.com/IntersectAustralia/dc21.git -b new_deploy
 cd $HOME/code_base/dc21
 
-curl -L http://get.rvm.io | bash -s stable --ruby=1.9.2-p290
-source $HOME/.rvm/scripts/rvm
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> $HOME/.bashrc
+# Set up RVM
+type -P $HOME/.rvm/scripts/rvm > /dev/null
+if [ $? -ne 0 ]; then
+  curl -L http://get.rvm.io | bash -s stable --ruby=1.9.2-p290
+  source $HOME/.rvm/scripts/rvm
+  echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> $HOME/.bashrc
+fi
+
 source $HOME/.bash_profile
 source $HOME/.bashrc
 cd $HOME/code_base/dc21
@@ -62,12 +70,33 @@ rvm use 1.9.2-p290
 rvm gemset create dc21app
 gem install bundler -v 1.0.20
 bundle install
+status=$?
 
-cap production_local server_setup:deploy_config
-cap production_local deploy:first_time
+if [ $status -eq 0 ]; then
+  cap production_local server_setup:deploy_config
+else
+  echo "$(tput setaf 1)ERROR $status: Bundle install failed$(tput sgr0)"
+  exit $status;
+fi
 
-echo "Please copy the following certificate to register for AAF"
-sudo cat /etc/shibboleth/sp-cert.pem
+status=$?
+if [ $status -eq 0 ]; then
+  cap production_local deploy:first_time
+else
+  echo "$(tput setaf 1)ERROR $status: deploy config set up failed.$(tput sgr0)"
+  exit $status;
+fi
 
-echo "Please remember to add your SSL certificate and key to /etc/httpd/ssl/server.crt and /etc/httpd/ssl/server.key respectively"
-echo "After adding your SSL certificate, run 'sudo service httpd restart'."
+status=$?
+if [ $status -eq 0 ]; then
+  echo "$(tput setaf 3)Please copy the following certificate to register for AAF$(tput sgr0)"
+  sudo cat /etc/shibboleth/sp-cert.pem
+
+  echo "$(tput setaf 3)Please remember to add your SSL certificate and key to /etc/httpd/ssl/server.crt and /etc/httpd/ssl/server.key respectively$(tput sgr0)"
+  echo "$(tput setaf 3)After adding your SSL certificate, run 'sudo service httpd restart'.$(tput sgr0)"
+else
+  echo "$(tput setaf 1)ERROR $status: The local deploy process failed. Please investigate and try again.$(tput sgr0)"
+  exit $status;
+fi
+
+
