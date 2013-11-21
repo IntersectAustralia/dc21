cf599111d2e1c6d7478df3359ac623b091dbfd13
danieltheodosius <danielt@intersect.org.au>
2013-11-22 10:12:09 +1100

    change the script

--- dc21_setup.sh
+++ dc21_setup.sh
@@ -1,6 +1,6 @@
 export DC21_HOST=localhost DC21_DB_PWD=dc21_test DC21_AAF_TEST=true DC21_TAG=snap-deploy PASSWORD="FT^yhu8ik" JOAI="Pass123" FIRST_NAME="John" LAST_NAME="Smith" EMAIL="admin@intersect.org.au" USER_PASS="Pass.123" YES_NO="yes"
 /usr/bin/expect <<EOD
-spawn `bash <(curl -s https://raw.github.com/IntersectAustralia/dc21/snap-deploy/vm_setup.sh)`
+spawn bash <(curl -s https://raw.github.com/IntersectAustralia/dc21/snap-deploy/vm_setup.sh)
 
 expect -re " password for devel:"
 send "$password\r"
@@ -35,4 +35,4 @@ send "$yes_no\r"
 expect -re " password for devel:"
 send "$password\r"
 interact
-EOD>>
+EOD
