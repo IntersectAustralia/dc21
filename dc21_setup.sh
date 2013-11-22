0b5e9f36f9c660eab2ffea4f65a0985076846f5b
jake-pf <jake@intersect.org.au>
2013-11-22 11:58:28 +1100

    Update dc21_setup.sh

--- dc21_setup.sh
+++ dc21_setup.sh
@@ -1,4 +1,4 @@
-export DC21_HOST=localhost DC21_DB_PWD=dc21_test DC21_AAF_TEST=true DC21_TAG=snap-deploy PASSWORD="FT^yhu8ik" JOAI="Pass123" FIRST_NAME="John" LAST_NAME="Smith" EMAIL="admin@intersect.org.au" USER_PASS="Pass.123" YES_NO="yes"
+export DC21_HOST=localhost DC21_DB_PWD=dc21_test DC21_AAF_TEST=true DC21_TAG=snap-deploy PASSWORD=FT^yhu8ik JOAI=Pass123 FIRST_NAME=John LAST_NAME=Smith EMAIL=admin@intersect.org.au USER_PASS=Pass.123 YES_NO=yes
 wget https://raw.github.com/IntersectAustralia/dc21/snap-deploy/vm_setup.sh
 /usr/bin/expect -<<EOD
 spawn bash vm_setup.sh
