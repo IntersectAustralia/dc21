09f0ac0f6387b7ae9de51157a521b8d674d69491
Shuqian Hon <sq@intersect.org.au>
2013-11-22 14:08:16 +1100

    EYETRACKER-MISC: Fix duplicate email presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_presence_of :email
   validates_presence_of :status
 
   validates_length_of :first_name, :maximum => 255
