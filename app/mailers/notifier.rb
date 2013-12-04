ac9b2ab88a844317142d25dd5812e35f9d913644
charles <charles@intersect.org.au>
2013-12-04 16:41:44 +1100

    EYETRACKER-164: Fix issue with system name updates

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,62 +1,65 @@
 class Notifier < ActionMailer::Base
 
-  SYSTEM_NAME = SystemConfiguration.instance.name
+  def system_name
+    config = Proc.new { return SystemConfiguration.instance.name }
+    config.call
+  end
 
   def notify_user_of_approved_request(recipient)
     @user = recipient
-    @system_name = SYSTEM_NAME
+    @system_name = system_name
     mail( :to => @user.email, 
           :from => APP_CONFIG['notification_email_sender'],
           :reply_to => APP_CONFIG['notification_email_sender'],
-          :subject => "#{SYSTEM_NAME} - Your access request has been approved")
+          :subject => "#{system_name} - Your access request has been approved")
   end
 
   def notify_user_of_rejected_request(recipient)
     @user = recipient
-    @system_name = SYSTEM_NAME
+    @system_name = system_name
     mail( :to => @user.email, 
           :from => APP_CONFIG['notification_email_sender'],
           :reply_to => APP_CONFIG['notification_email_sender'],
-          :subject => "#{SYSTEM_NAME} - Your access request has been rejected")
+          :subject => "#{system_name} - Your access request has been rejected")
   end
 
   # notifications for super users
   def notify_superusers_of_access_request(applicant)
     superusers_emails = User.get_superuser_emails
     @user = applicant
-    @system_name = SYSTEM_NAME
+    @system_name = system_name
     mail( :to => superusers_emails,
           :from => APP_CONFIG['notification_email_sender'],
           :reply_to => @user.email,
-          :subject => "#{SYSTEM_NAME} - There has been a new access request")
+          :subject => "#{system_name} - There has been a new access request")
   end
 
   def notify_user_that_they_cant_reset_their_password(user)
     @user = user
-    @system_name = SYSTEM_NAME
+    @system_name = system_name
     mail( :to => @user.email,
           :from => APP_CONFIG['notification_email_sender'],
           :reply_to => APP_CONFIG['notification_email_sender'],
-          :subject => "#{SYSTEM_NAME} - Reset password instructions")
+          :subject => "#{system_name} - Reset password instructions")
   end
 
   def notify_user_of_completed_package(package)
     @user = package.created_by
-    @system_name = SYSTEM_NAME
+    @system_name = system_name
     @package = package
     mail( :to => @user.email,
           :from => APP_CONFIG['notification_email_sender'],
           :reply_to => APP_CONFIG['notification_email_sender'],
-          :subject => "#{SYSTEM_NAME} - Package completed")
+          :subject => "#{system_name} - Package completed")
   end
 
   def notify_user_reset_password_instructions(recipient)
     @user = recipient
-    @system_name = SYSTEM_NAME
+    @system_name = system_name
     mail( :to => @user.email,
           :from => APP_CONFIG['notification_email_sender'],
           :reply_to => APP_CONFIG['notification_email_sender'],
-          :subject => "#{SYSTEM_NAME} - Reset password instructions")
+          :subject => "#{system_name} - Reset password instructions")
   end
 
 end
