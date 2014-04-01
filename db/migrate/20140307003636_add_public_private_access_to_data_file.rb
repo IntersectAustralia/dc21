d3e657864d807c059c02f4c7c001a46df5631e8c
Cindy <cindy@intersect.org.au>
2014-04-01 17:18:40 +1100

    UWSDEV-56: Ensure access columns are removed from data files in db rollback to v2.0

--- db/migrate/20140307003636_add_public_private_access_to_data_file.rb
+++ db/migrate/20140307003636_add_public_private_access_to_data_file.rb
@@ -1,7 +1,12 @@
 class AddPublicPrivateAccessToDataFile < ActiveRecord::Migration
-  def change
+  def up
     add_column :data_files, :access, :text
     add_column :data_files, :access_to_all_institutional_users, :bool
     add_column :data_files, :access_to_user_groups, :bool
+  end
+  def down
+    remove_column :data_files, :access
+    remove_column :data_files, :access_to_all_institutional_users
+    remove_column :data_files, :access_to_user_groups
   end
 end
