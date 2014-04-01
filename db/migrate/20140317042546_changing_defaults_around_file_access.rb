01b94f0f1f324b60d8987dc24b436e536b5983b3
Cindy <cindy@intersect.org.au>
2014-04-01 16:14:11 +1100

    UWSDEV-56: Set default access values in migration for existing rows, and add down method for db rollback.

--- db/migrate/20140317042546_changing_defaults_around_file_access.rb
+++ db/migrate/20140317042546_changing_defaults_around_file_access.rb
@@ -1,6 +1,11 @@
 class ChangingDefaultsAroundFileAccess < ActiveRecord::Migration
-  def change
-      change_column_default :data_files, :access, "Private"
-      change_column_default :data_files, :access_to_all_institutional_users, true
+  def up
+    change_column_default :data_files, :access, "Private"
+    change_column_default :data_files, :access_to_all_institutional_users, true
+    DataFile.update_all(access: "Private", access_to_all_institutional_users: true)
+  end
+  def down
+    change_column_default :data_files, :access, ""
+    change_column_default :data_files, :access_to_all_institutional_users, nil
   end
 end
