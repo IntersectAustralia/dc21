1091d1a798df698c97f89972debc5f2f33a63f16
Shuqian Hon <sq@intersect.org.au>
2014-10-16 11:37:21 +1100

    Fix devise migration

--- db/migrate/20111103002408_devise_create_users.rb
+++ db/migrate/20111103002408_devise_create_users.rb
@@ -37,7 +37,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
         t.timestamps
       end
-    end
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
--- db/schema.rb
+++ db/schema.rb
@@ -275,24 +275,24 @@ ActiveRecord::Schema.define(:version => 20140414001925) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",                       :default => 0
+    t.integer  "failed_attempts",        :default => 0
     t.datetime "locked_at"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "status"
     t.integer  "role_id"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "authentication_token"
   end
 
