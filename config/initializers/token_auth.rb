9c26340c6e7b7775a4d1f1a425da2e280c9bbb4a
Veronica Luke <veronica@intersect.org.au>
2015-02-26 09:18:01 +1100

    DIVERBEVAN-50 Added get list of variables API call

--- config/initializers/token_auth.rb
+++ config/initializers/token_auth.rb
@@ -4,7 +4,7 @@ module Devise
   module Strategies
     class TokenAuthenticatable
       def valid?
-        super && params[:controller] == 'data_files' && (params[:action] == 'download' || params[:action] == 'api_create' || params[:action] == 'api_search')
+        super && params[:controller] == 'data_files' && (params[:action] == 'download' || params[:action] == 'api_create' || params[:action] == 'api_search' || params[:action] == 'variable_list')
       end
     end
   end
