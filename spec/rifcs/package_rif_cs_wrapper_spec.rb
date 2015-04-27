9a0e8ce5ae839544d9b2e718efd40dc39969c6e6
Cindy <cindy@intersect.org.au>
2015-04-27 15:35:03 +1000

    DIVERBEVAN-60: Fix path in data files in unit tests.

--- spec/rifcs/package_rif_cs_wrapper_spec.rb
+++ spec/rifcs/package_rif_cs_wrapper_spec.rb
@@ -120,6 +120,8 @@ describe PackageRifCsWrapper do
   end
 
   describe "Rights" do
+    data_file_path = Rails.root.to_s + "/tmp/a-path"
+    `touch #{data_file_path}`
 
     it "should return the rights from the experiment associated with the package and not the files" do
       exp1 = Factory(:experiment, :access_rights => "http://creativecommons.org/licenses/by/3.0/au")
@@ -127,13 +129,13 @@ describe PackageRifCsWrapper do
       exp3 = Factory(:experiment, :access_rights => "http://creativecommons.org/licenses/by-nd/3.0/au")
       exp_reserved = Factory(:experiment, :access_rights => "N/A")
 
-      df1 = Factory(:data_file, :experiment_id => exp1.id)
-      df2 = Factory(:data_file, :experiment_id => exp2.id)
-      df3 = Factory(:data_file, :experiment_id => exp1.id)
-      df4 = Factory(:data_file, :experiment_id => exp2.id)
-      df_reserved = Factory(:data_file, :experiment_id => exp_reserved.id)
+      df1 = Factory(:data_file, :experiment_id => exp1.id, :path => data_file_path)
+      df2 = Factory(:data_file, :experiment_id => exp2.id, :path => data_file_path)
+      df3 = Factory(:data_file, :experiment_id => exp1.id, :path => data_file_path)
+      df4 = Factory(:data_file, :experiment_id => exp2.id, :path => data_file_path)
+      df_reserved = Factory(:data_file, :experiment_id => exp_reserved.id, :path => data_file_path)
 
-      package = Factory(:package, :experiment_id => exp3.id, :filename => 'open package')
+      package = Factory(:package, :experiment_id => exp3.id, :filename => 'open package', :path => data_file_path)
       CustomDownloadBuilder.bagit_for_files_with_ids([df1.id, df2.id, df3.id, df4.id, df_reserved.id], package) do |zip_file|
         attachment_builder = AttachmentBuilder.new(APP_CONFIG['files_root'], nil, nil, nil)
         files = attachment_builder.build_package(package, zip_file)
@@ -149,12 +151,12 @@ describe PackageRifCsWrapper do
       exp2 = Factory(:experiment, :access_rights => "http://creativecommons.org/licenses/by-nc-sa/3.0/au")
       exp_reserved = Factory(:experiment, :access_rights => "N/A")
 
-      df1 = Factory(:data_file, :experiment_id => exp1.id)
-      df2 = Factory(:data_file, :experiment_id => exp2.id)
-      df3 = Factory(:data_file, :experiment_id => exp1.id)
-      df4 = Factory(:data_file, :experiment_id => exp2.id)
+      df1 = Factory(:data_file, :experiment_id => exp1.id, :path => data_file_path)
+      df2 = Factory(:data_file, :experiment_id => exp2.id, :path => data_file_path)
+      df3 = Factory(:data_file, :experiment_id => exp1.id, :path => data_file_path)
+      df4 = Factory(:data_file, :experiment_id => exp2.id, :path => data_file_path)
 
-      package = Factory(:package, :experiment_id => exp_reserved.id, :filename => 'non-open package')
+      package = Factory(:package, :experiment_id => exp_reserved.id, :filename => 'non-open package', :path => data_file_path)
       CustomDownloadBuilder.bagit_for_files_with_ids([df1.id, df2.id, df3.id, df4.id], package) do |zip_file|
         attachment_builder = AttachmentBuilder.new(APP_CONFIG['files_root'], nil, nil, nil)
         files = attachment_builder.build_package(package, zip_file)
