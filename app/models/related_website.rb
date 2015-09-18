f973aec85f001a951ff7d7f95c26ea0bee8626d7
Daniel Yazbek <daniel@intersect.org.au>
2015-09-18 12:28:07 +1000

    UWSHIEVMOD-96 API functionality to modify current records

--- app/models/related_website.rb
+++ app/models/related_website.rb
@@ -10,11 +10,16 @@ class RelatedWebsite < ActiveRecord::Base
   has_many :data_file_related_websites
 
   before_validation :remove_white_spaces
+  before_validation :strip_single_quotes
   default_scope order(:url)
 
   def remove_white_spaces
     self.url = self.url.to_s.strip
   end
 
+  def strip_single_quotes
+    self.url = self.url.slice(1..-1) if self.url.start_with?("'")
+    self.url = self.url.slice(0..-2) if self.url.ends_with?("'")
+  end
 
 end
